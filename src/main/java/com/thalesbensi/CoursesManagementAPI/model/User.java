ac0aac03589e01f684f487f758e48d36d6e32954
thalesbensi <thalesbensi@outlook.com>
2025-04-21 14:26:48 -0300

    feat: modify user entity

--- src/main/java/com/thalesbensi/CoursesManagementAPI/model/User.java
+++ src/main/java/com/thalesbensi/CoursesManagementAPI/model/User.java
@@ -2,6 +2,8 @@ package com.thalesbensi.CoursesManagementAPI.model;
 
 import com.thalesbensi.CoursesManagementAPI.enums.UserRole;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Entity
@@ -14,16 +16,23 @@ public class User {
     private Long userId;
 
     @Column(unique = true, nullable = false)
+    @NotNull
+    @NotBlank
     private String userName;
 
     @Column(unique = true, nullable = false)
+    @NotNull
+    @NotBlank
     private String email;
 
     @Column(unique = false, nullable = false)
+    @NotNull
+    @NotBlank
     private String password;
 
     @Enumerated(EnumType.STRING)
     @Column(unique = false, nullable = false)
+    @NotNull
     private UserRole userRole;
 
 }
