5ce15317d56eefb9492cc7a7bd033b0b4995de2a
thalesbensi <thalesbensi@outlook.com>
2025-04-20 22:46:02 -0300

    Project Setup

---
+++ src/main/java/com/thalesbensi/CoursesManagementAPI/CoursesManagementApiApplication.java
@@ -0,0 +1,13 @@
+package com.thalesbensi.CoursesManagementAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoursesManagementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoursesManagementApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thalesbensi/CoursesManagementAPI/CoursesManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thalesbensi.CoursesManagementAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoursesManagementApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
