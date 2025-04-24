e026c540f958af691fbf449886ecff805775feae
thalesbensi <thalesbensi@outlook.com>
2025-04-24 00:29:32 -0300

    feat(Course Entity): Implement DTOs to help data formatting to user

---
+++ src/main/java/com/thalesbensi/CoursesManagementAPI/dto/Course/CourseCreationTemplateDTO.java
@@ -0,0 +1,4 @@
+package com.thalesbensi.CoursesManagementAPI.dto.Course;
+
+public record CourseCreationTemplateDTO(Long id, String title, String description, Long teacherId){
+}
---
+++ src/main/java/com/thalesbensi/CoursesManagementAPI/dto/Course/CourseResponseTemplateDTO.java
@@ -0,0 +1,19 @@
+package com.thalesbensi.CoursesManagementAPI.dto.Course;
+
+import com.thalesbensi.CoursesManagementAPI.model.Course;
+import com.thalesbensi.CoursesManagementAPI.model.User;
+
+import java.util.Date;
+
+public record CourseResponseTemplateDTO(Long id, String title, String description, Long teacher_id, String teacherName, Date creationDate) {
+
+    public static CourseResponseTemplateDTO fromEntity(Course course, User teacher) {
+        return new CourseResponseTemplateDTO(
+                course.getId(),
+                course.getTitle(),
+                course.getDescription(),
+                teacher.getId(),
+                teacher.getName(),
+                course.getCreationDate());
+    }
+}
