65c0b6a4590b08b41244040411ff52c796a0ff4a
thalesbensi <thalesbensi@outlook.com>
2025-04-24 16:37:19 -0300

    feat: Create Lesson Controller

---
+++ src/main/java/com/thalesbensi/CoursesManagementAPI/controller/LessonController.java
@@ -0,0 +1,44 @@
+package com.thalesbensi.CoursesManagementAPI.controller;
+
+import com.thalesbensi.CoursesManagementAPI.dto.Lesson.LessonDTO;
+import com.thalesbensi.CoursesManagementAPI.model.Lesson;
+import com.thalesbensi.CoursesManagementAPI.services.LessonService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/lessons")
+public class LessonController {
+
+    private final LessonService lessonService;
+    public LessonController(LessonService lessonService) {this.lessonService = lessonService;}
+
+    @GetMapping()
+    public ResponseEntity<List<LessonDTO>> getAllLessons() {
+        return ResponseEntity.status(HttpStatus.OK).body(lessonService.getAllLessons());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<LessonDTO> getLessonById(@PathVariable Long id) {
+        return ResponseEntity.status(HttpStatus.OK).body(lessonService.getLessonById(id));
+    }
+
+    @PostMapping()
+    public ResponseEntity<LessonDTO> createLesson(@RequestBody Lesson lesson) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(lessonService.createLesson(lesson));
+    }
+
+    @PutMapping()
+    public ResponseEntity<LessonDTO> updateLesson(@RequestBody Lesson lesson, Long id) {
+        return ResponseEntity.status(HttpStatus.OK).body(lessonService.updateLesson(lesson,id));
+    }
+
+    @DeleteMapping()
+    public ResponseEntity<LessonDTO> deleteLesson(Long id) {
+        lessonService.deleteLesson(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
+}
