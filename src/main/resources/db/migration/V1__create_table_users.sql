4a1da19f74a5d703651f30a214e9719e55935652
thalesbensi <thalesbensi@outlook.com>
2025-05-07 17:15:28 -0300

    fix: Fix a query

--- src/main/resources/db/migration/V1__create_table_users.sql
+++ src/main/resources/db/migration/V1__create_table_users.sql
@@ -1,5 +1,5 @@
 CREATE TABLE user_tb (
-        id BIGSERIAL PRIMARY KEY,
+        id BIGSERIAL PRIMARY KEY UNIQUE NOT NULL,
         name VARCHAR(255) NOT NULL ,
         email VARCHAR(255) UNIQUE NOT NULL,
         password VARCHAR(255) NOT NULL,
