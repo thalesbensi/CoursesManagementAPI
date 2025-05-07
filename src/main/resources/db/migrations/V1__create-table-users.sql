df1c97174f1146c5313429700000de3f876b2f34
thalesbensi <thalesbensi@outlook.com>
2025-05-07 09:39:22 -0300

    fix: Fix a spelling error

--- src/main/resources/db/migrations/V1__create-table-users.sql
+++ src/main/resources/db/migrations/V1__create-table-users.sql
@@ -3,5 +3,5 @@ CREATE TABLE user_tb (
         name VARCHAR(255) NOT NULL ,
         email VARCHAR(255) UNIQUE NOT NULL,
         password VARCHAR(255) NOT NULL,
-        role VARCHAR(10) CHECK (papel IN ('TEACHER', 'STUDENT', 'ADMIN'))
+        role VARCHAR(10) CHECK (role IN ('TEACHER', 'STUDENT', 'ADMIN'))
 );
