1ae1966bc42c5e87ef9735834cb95362d4556770
thalesbensi <thalesbensi@outlook.com>
2025-05-08 12:43:28 -0300

    feat: Implement Registration/Login with JWT token generation

--- src/main/java/com/thalesbensi/CoursesManagementAPI/api/controller/AuthenticationController.java
+++ src/main/java/com/thalesbensi/CoursesManagementAPI/api/controller/AuthenticationController.java
@@ -3,13 +3,14 @@ package com.thalesbensi.CoursesManagementAPI.api.controller;
 
 import com.thalesbensi.CoursesManagementAPI.api.dto.authentication.AuthenticationDTO;
 import com.thalesbensi.CoursesManagementAPI.api.dto.authentication.RegisterDTO;
+import com.thalesbensi.CoursesManagementAPI.api.dto.authentication.response.LoginResponseDTO;
 import com.thalesbensi.CoursesManagementAPI.domain.entity.User;
 import com.thalesbensi.CoursesManagementAPI.domain.repositories.UserRepository;
+import com.thalesbensi.CoursesManagementAPI.infrastructure.security.TokenService;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.crypto.bcrypt.BCrypt;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
@@ -19,17 +20,20 @@ public class AuthenticationController {
 
     private final AuthenticationManager authenticationManager;
     private final UserRepository userRepository;
+    private final TokenService tokenService;
 
-    public AuthenticationController(AuthenticationManager authenticationManager, UserRepository userRepository) {
+    public AuthenticationController(AuthenticationManager authenticationManager, UserRepository userRepository, TokenService tokenService) {
         this.authenticationManager = authenticationManager;
         this.userRepository = userRepository;
+        this.tokenService = tokenService;
     }
 
     @PostMapping("/login")
     public ResponseEntity login(@RequestBody @Valid AuthenticationDTO data) {
             var usernamePassword = new UsernamePasswordAuthenticationToken(data.email(), data.password());
             var authentication = this.authenticationManager.authenticate(usernamePassword);
-            return ResponseEntity.ok().build();
+            var token = tokenService.generateToken((User)authentication.getPrincipal());
+            return ResponseEntity.ok(new LoginResponseDTO(token));
     }
 
     @PostMapping("/register")
