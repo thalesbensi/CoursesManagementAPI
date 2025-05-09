f1a452b19517dfc4f18d3401865ac3fa5ac558d8
thalesbensi <thalesbensi@outlook.com>
2025-05-09 14:17:34 -0300

    fix: Alter table name on migrations

--- src/main/resources/db/migration/V4__create_table_enrollment.sql
+++ src/main/resources/db/migration/V4__create_table_enrollment.sql
@@ -2,7 +2,7 @@ CREATE TABLE enrollment_tb (
         id BIGSERIAL PRIMARY KEY,
         student_id BIGINT NOT NULL,
         course_id BIGINT NOT NULL,
-        enrollmentDate DATE NOT NULL,
+        enrollment_date DATE NOT NULL,
         FOREIGN KEY (student_id) REFERENCES user_tb (id),
         FOREIGN KEY (course_id) REFERENCES course_tb (id)
 );
