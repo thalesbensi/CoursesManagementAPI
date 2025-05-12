98233df681a8457973af3a1a96cad6e887ad06d1
thalesbensi <thalesbensi@outlook.com>
2025-05-12 15:56:43 -0300

    fix: Do a few modifications on Lesson DTOs and Mapper

---
+++ src/main/java/com/thalesbensi/CoursesManagementAPI/api/dto/response/LessonListItemResponseDTO.java
@@ -0,0 +1,4 @@
+package com.thalesbensi.CoursesManagementAPI.api.dto.response;
+
+public record LessonListItemResponseDTO(Long id, String title, String description, String urlVideo) {
+}
--- src/main/java/com/thalesbensi/CoursesManagementAPI/infrastructure/mapper/LessonMapper.java
+++ src/main/java/com/thalesbensi/CoursesManagementAPI/infrastructure/mapper/LessonMapper.java
@@ -2,16 +2,23 @@ package com.thalesbensi.CoursesManagementAPI.infrastructure.mapper;
 
 import com.thalesbensi.CoursesManagementAPI.api.dto.LessonDTO;
 import com.thalesbensi.CoursesManagementAPI.api.dto.request.LessonRequestDTO;
+import com.thalesbensi.CoursesManagementAPI.api.dto.response.LessonListItemResponseDTO;
+import com.thalesbensi.CoursesManagementAPI.domain.entity.Course;
 import com.thalesbensi.CoursesManagementAPI.domain.entity.Lesson;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import org.mapstruct.Mappings;
 
 @Mapper(componentModel = "spring")
 public interface LessonMapper {
 
+    @Mapping(target = "course", source = "course")
     LessonDTO toDTO(Lesson lesson);
 
-    @Mappings({@Mapping(target = "course.id", source = "course")})
-    Lesson requestDTOtoEntity(LessonRequestDTO lessonRequestDTO);
+    @Mapping(target = "course", source = "course")
+    @Mapping(target = "title", source = "lessonRequestDTO.title")
+    @Mapping(target = "description", source = "lessonRequestDTO.description")
+    @Mapping(target = "urlVideo", source = "lessonRequestDTO.urlVideo")
+    Lesson requestDTOtoEntity(LessonRequestDTO lessonRequestDTO, Course course);
+
+    LessonListItemResponseDTO fromLessonToLessonListItemResponseDTO(Lesson lesson);
 }
