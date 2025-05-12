4ac3058115612ecd910d268ec95043efdc443c7f
thalesbensi <thalesbensi@outlook.com>
2025-05-12 15:54:55 -0300

    feat: Refactor User Scope to respect new authentication and authorization logic on methods

--- src/main/java/com/thalesbensi/CoursesManagementAPI/api/controller/UserController.java
+++ src/main/java/com/thalesbensi/CoursesManagementAPI/api/controller/UserController.java
@@ -1,10 +1,7 @@
 package com.thalesbensi.CoursesManagementAPI.api.controller;
 
-import com.thalesbensi.CoursesManagementAPI.api.dto.request.UserRequestDTO;
 import com.thalesbensi.CoursesManagementAPI.api.dto.response.UserMinResponseDTO;
-import com.thalesbensi.CoursesManagementAPI.api.dto.response.UserResponseDTO;
 import com.thalesbensi.CoursesManagementAPI.domain.services.UserService;
-import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
--- src/main/java/com/thalesbensi/CoursesManagementAPI/domain/repositories/UserRepository.java
+++ src/main/java/com/thalesbensi/CoursesManagementAPI/domain/repositories/UserRepository.java
@@ -4,7 +4,10 @@ import com.thalesbensi.CoursesManagementAPI.domain.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> { 
 
     UserDetails findByEmail(String email);
+    Optional<User> findUserByEmail(String email);
 }
