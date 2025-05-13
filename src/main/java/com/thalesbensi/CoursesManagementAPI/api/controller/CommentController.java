4cf24a8d76a63f6af6f08aa6b7024e8749e04d01
thalesbensi <thalesbensi@outlook.com>
2025-05-13 12:29:23 -0300

    feat: Implement Comment Controller

---
+++ src/main/java/com/thalesbensi/CoursesManagementAPI/api/controller/CommentController.java
@@ -0,0 +1,43 @@
+package com.thalesbensi.CoursesManagementAPI.api.controller;
+
+import com.thalesbensi.CoursesManagementAPI.api.dto.request.CommentRequestDTO;
+import com.thalesbensi.CoursesManagementAPI.api.dto.response.CommentResponseDTO;
+import com.thalesbensi.CoursesManagementAPI.domain.services.CommentService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/comments")
+public class CommentController {
+
+    private final CommentService commentService;
+
+    public CommentController(CommentService commentService) {
+        this.commentService = commentService;
+    }
+
+    @GetMapping("lesson/{lessonId}")
+    public ResponseEntity<List<CommentResponseDTO>> getCommentsOnLesson(@PathVariable Long lessonId){
+        return ResponseEntity.status(HttpStatus.OK).body(commentService.getCommentsOnLesson(lessonId));
+    }
+
+    @PostMapping("lesson/{lessonId}")
+    public ResponseEntity addCommentToLesson(@PathVariable Long lessonId, @RequestBody CommentRequestDTO commentRequestDTO){
+        return ResponseEntity.status(HttpStatus.CREATED).body(commentService.addCommentToLesson(lessonId, commentRequestDTO));
+    }
+
+    @PutMapping("/{commentId}")
+    public ResponseEntity updateLesson(@PathVariable Long commentId, @RequestBody CommentRequestDTO commentRequestDTO){
+        return ResponseEntity.status(HttpStatus.OK).body(commentService.updateComment(commentId, commentRequestDTO));
+    }
+
+    @DeleteMapping("/{commentId}")
+    public ResponseEntity deleteComment(@PathVariable Long commentId){
+        commentService.deleteComment(commentId);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
+}
