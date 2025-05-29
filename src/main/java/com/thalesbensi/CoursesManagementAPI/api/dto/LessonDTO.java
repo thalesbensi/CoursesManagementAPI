081d9e789d8c3c8213da97594b94728ef11efe8b
thalesbensi <thalesbensi@outlook.com>
2025-05-29 12:49:36 -0300

    fix: Rename imports

--- src/main/java/com/thalesbensi/CoursesManagementAPI/api/dto/LessonDTO.java
+++ src/main/java/com/thalesbensi/CoursesManagementAPI/api/dto/LessonDTO.java
@@ -1,5 +1,5 @@
 package com.thalesbensi.CoursesManagementAPI.api.dto;
 
-import com.thalesbensi.CoursesManagementAPI.api.dto.response.CourseResponseDTO;
+import com.thalesbensi.CoursesManagementAPI.api.dto.response.course.CourseResponseDTO;
 
 public record LessonDTO(Long id, String title, String description, String urlVideo, CourseResponseDTO course) { }
